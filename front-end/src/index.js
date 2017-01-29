4d231c6e326e484bc1a72305957b70f620d858a5
Hoang Kien <kienhg96@gmail.com>
2017-01-29 18:02:45 +0700

    Add Routes

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import '../public/css/style.css';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Provider } from 'react-redux';
 import store from './store/config';
+import { Router, Route, browserHistory, hashHistory , IndexRoute } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
 
-injectTapEventPlugin();
+const history = syncHistoryWithStore(hashHistory, store);
+
 ReactDOM.render(
   	<Provider store={store}>
-  		<App />
+  		<Router history={history}>
+            <Route path="/">
+				<IndexRoute component={App}/>
+				<Route path="login" component={Login}/>
+				<Route path="signup" component={Signup}/>
+            </Route>
+        </Router>
   	</Provider>,
   	document.getElementById('root')
 );
